d1c2b384e855732ea5155ff668f684c26b0090cc
Jan van Brügge <supermanitu@gmail.com>
2020-01-26 18:32:52 +0100

    Remove code duplication in oracle tests

--- test/arrayOracle.ts
+++ test/arrayOracle.ts
@@ -3,11 +3,14 @@ import * as fc from 'fast-check';
 
 import { map, fromArray, subscribe, pipe } from '../src/index';
 
-describe('map()', () => {
-  it('should behave like Array.map()', () => {
+describe('using Array as oracle', () => {
+  function mkOracleTest<T extends (...args: any[]) => any>(
+    fnArbitrary: fc.Arbitrary<T>,
+    wrapFn: (f: T) => (arg: any) => any = f => x => f(x)
+  ) {
     fc.assert(
-      fc.property(fc.array(fc.anything()), fc.func(fc.anything()), (arr, f) => {
-        const oracle = arr.map(x => f(x));
+      fc.property(fc.array(fc.anything()), fnArbitrary, (arr, f) => {
+        const oracle = arr.map(wrapFn(f));
 
         let res: any[] = [];
 
@@ -15,7 +18,7 @@ describe('map()', () => {
 
         pipe(
           fromArray(arr),
-          map(f),
+          map(wrapFn(f)),
           subscribe({
             next: x => res.push(x),
             complete: () => {
@@ -28,5 +31,13 @@ describe('map()', () => {
         assert.strictEqual(completed, 1);
       })
     );
+  }
+
+  it('map()', () => {
+    mkOracleTest(fc.func(fc.anything()));
+  });
+
+  it('filter()', () => {
+    mkOracleTest(fc.compareBooleanFunc(), f => x => f(x, 0));
   });
 });
