8a930951f8e5577570ed5a9510e20aae0dc7775a
Jan van Brügge <supermanitu@gmail.com>
2020-01-26 16:22:12 +0100

    Add first property test

--- test/map.ts
+++ test/map.ts
@@ -1,7 +1,27 @@
+import * as assert from 'assert';
 import * as fc from 'fast-check';
 
-describe('mocha test', () => {
-  it('simple test', () => {
-    fc.assert(fc.property(fc.string(), text => text.indexOf(text) === 0));
+import { map, fromArray, forEach, pipe } from '../src/index';
+
+describe('map tests', () => {
+  describe('oracle tests', () => {
+    it('should handle simple *2 function', () => {
+      fc.assert(
+        fc.property(fc.array(fc.integer(), 0, 100), arr => {
+          const f = (x: number) => x * 2;
+          const oracle = arr.map(f);
+
+          let res: number[] = [];
+
+          pipe(
+            fromArray(arr),
+            map(f),
+            forEach(x => res.push(x))
+          );
+
+          assert.deepStrictEqual(oracle, res);
+        })
+      );
+    });
   });
 });
