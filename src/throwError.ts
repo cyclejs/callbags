6a337b78ab9cd0987b0512b11649c1c2fc543fbe
Andre Staltz <andre@staltz.com>
2020-02-11 14:58:39 +0200

    fix throwError to pass a talkback downwards

--- src/throwError.ts
+++ src/throwError.ts
@@ -2,7 +2,7 @@ import { Source } from './types';
 
 export function throwError<T>(error: any): Source<T> {
   return (_, sink) => {
-    sink(0);
+    sink(0, () => {});
     sink(2, error);
   };
 }
---
+++ test/throwError.ts
@@ -0,0 +1,26 @@
+import * as assert from 'assert';
+
+import {
+  pipe,
+  take,
+  throwError,
+  subscribe
+} from '../src/index';
+
+describe('throwError', () => {
+  it('should give a talkback when subscribed', () => {
+    let taken = 0;
+
+    pipe(
+      throwError(new Error('foo')),
+      take(0),
+      subscribe({
+        next: () => {
+          taken++;
+        }
+      })
+    );
+
+    assert.strictEqual(taken, 0);
+  });
+});
