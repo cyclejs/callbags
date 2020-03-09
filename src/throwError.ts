532226dc2142575849e1b1013e47572bbc69bbae
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-03-09 11:22:54 +0100

    Do not emit from throwError after immediate (sync) disposal from the consumer

--- src/throwError.ts
+++ src/throwError.ts
@@ -2,7 +2,11 @@ import { Producer } from './types';
 
 export function throwError<T>(error: any): Producer<T> {
   return (_, sink) => {
-    sink(0, () => {});
+    let ended = false;
+    sink(0, () => {
+      ended = true;
+    });
+    if (ended) return;
     sink(2, error);
   };
 }
--- test/throwError.ts
+++ test/throwError.ts
@@ -16,4 +16,21 @@ describe('throwError', () => {
 
     assert.strictEqual(taken, 0);
   });
+
+  it('should not notify about error after synchronous disposal', () => {
+    let taken = 0;
+
+    pipe(throwError(new Error('foo')), source => {
+      source(0, (t, d) => {
+        if (t === 0) {
+          d(2);
+          return;
+        }
+        taken++;
+      });
+      return () => {};
+    });
+
+    assert.strictEqual(taken, 0);
+  });
 });
