3218d0ea305a5f4cee77841ac0b5411c53182d84
Jan van Brügge <supermanitu@gmail.com>
2020-04-21 15:39:15 +0200

    Add tests for the hooks

---
+++ test/hooks.ts
@@ -0,0 +1,83 @@
+import * as assert from 'assert';
+
+import {
+  uponStart,
+  uponEnd,
+  makeSubject,
+  pipe,
+  subscribe,
+  of,
+  throwError
+} from '../src/index';
+
+describe('hooks', () => {
+  it('uponStart should be called when the first listener subscribes', () => {
+    const subject = makeSubject<number>();
+
+    subject(1, 0);
+
+    let res: any[] = [];
+    let effect = false;
+    pipe(
+      subject,
+      uponStart(() => {
+        effect = true;
+        assert.deepStrictEqual(res, []);
+      }),
+      subscribe(
+        d => {
+          res.push(d);
+        },
+        e => {
+          if (e) assert.fail('should not terminate with an error');
+        }
+      )
+    );
+
+    subject(1, 2);
+    subject(1, 3);
+
+    assert.strictEqual(effect, true);
+    assert.deepStrictEqual(res, [2, 3]);
+  });
+
+  it('uponEnd should be called when the stream terminates normally', () => {
+    let effect = false;
+    let completed = false;
+    pipe(
+      of(5),
+      uponEnd(() => {
+        effect = true;
+      }),
+      subscribe(
+        d => assert.strictEqual(d, 5),
+        e => {
+          if (e) assert.fail('should not terminate with an error');
+          assert.strictEqual(effect, true);
+          completed = true;
+        }
+      )
+    );
+    assert.strictEqual(completed, true);
+  });
+
+  it('uponEnd should be called when the stream terminates with an error', () => {
+    let effect = false;
+    let completed = false;
+    pipe(
+      throwError('myError'),
+      uponEnd(() => {
+        effect = true;
+      }),
+      subscribe(
+        d => assert.strictEqual(d, 5),
+        e => {
+          assert.strictEqual(e, 'myError');
+          assert.strictEqual(effect, true);
+          completed = true;
+        }
+      )
+    );
+    assert.strictEqual(completed, true);
+  });
+});
